6da2c6d5ee2610c5e16f49a0d0af325882780282
Omri Litov <omrilitov@gmail.com>
2017-08-25 10:53:19 +0300

    update to webpack 3

--- webpack/dev.config.babel.js
+++ webpack/dev.config.babel.js
@@ -1,20 +1,115 @@
+import 'dotenv/config';
 import {resolve} from 'path';
+import webpack from 'webpack';
+import _ from 'lodash';
+import nodeExternals from 'webpack-node-externals';
 
-export default {
+const webpackPort = process.env.WEBPACK_PORT;
+
+const config = {
   context: resolve(__dirname, '..'),
   output: {
     path: resolve(__dirname, '../dist')
   },
+  resolve: {
+    modules: ['node_modules', 'src'],
+    extensions: ['.json', '.js', '.jsx']
+  },
   module: {
-    loaders: [
-      {test: /\.less$/, loader: 'isomorphic-style!css?modules!less'},
-      {test: /\.css$/, loader: 'isomorphic-style!css'},
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', 'babel']},
-      {test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000'}
+    rules: [
+      {
+        test: /\.less$/,
+        rules: [{
+          loader: 'isomorphic-style-loader'
+        }, {
+          loader: 'css-loader',
+          options: {
+            modules: true
+          }
+        }, {
+          loader: 'less-loader'
+        }]
+      },
+      {
+        test: /\.css/,
+        rules: [{
+          loader: 'isomorphic-style-loader'
+        }, {
+          loader: 'css-loader'
+        }]
+      },
+      {
+        test: /\.js$/,
+        include: resolve(__dirname, '../src'),
+        rules: [
+          /*{
+            loader: 'react-hot-loader'
+          }, */{
+            loader: 'babel-loader'
+          }]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10000
+          }
+        }]
+      }
+    ]
+  }
+};
+
+const clientConfig = _.defaultsDeep({
+  name: 'client',
+  target: 'web',
+  entry: {
+    client: [
+      'react-hot-loader/patch',
+      `webpack-dev-server/client?path=http://localhost:${webpackPort}`,
+      './src/client.js'
     ]
   },
-  resolve: {
-    modulesDirectories: ['node_modules'],
-    extensions: ['', '.json', '.js', '.jsx']
-  }
-}
+  output: {
+    filename: 'bundle.js',
+    publicPath: `http://localhost:${webpackPort}/dist/`
+  },
+  devServer: {
+    port: webpackPort,
+    contentBase: '/',
+    quiet: true,
+    noInfo: true,
+    hot: true,
+    inline: true,
+    lazy: false,
+    publicPath: `http://localhost:${webpackPort}/dist/`,
+    headers: {'Access-Control-Allow-Origin': '*'},
+    stats: {colors: true}
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        WEBPACK_ENV: '\'client\''
+      }
+    })
+  ]
+}, config);
+
+const serverConfig = _.defaultsDeep({
+  name: 'server',
+  target: 'node',
+  entry: {
+    server: [
+      './src/server.js'
+    ]
+  },
+  output: {
+    filename: 'server.js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: [nodeExternals()]
+}, config);
+
+export default [clientConfig, serverConfig];
